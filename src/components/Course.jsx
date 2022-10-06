8f2202a7bbb60ac5f720b8df4d2418d695cf224d
Cynthia <Liangshao2023@u.northwestern.edu>
2022-10-05 21:03:24 -0500

    catch time conflict

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,6 +1,6 @@
-const Course = ({id, course, selected, toggleSelected}) => (
-    <div className="card m-1 p-2" onClick={() => toggleSelected(course)}>
-        <div className={`card-body ${selected.includes(course) ? 'selected' : ''}`}>
+const Course = ({id, course, selected, toggleSelected, disabled}) => (
+    <div className="card m-1 p-2" onClick={() => disabled.includes(course) ? null : toggleSelected(course)}>
+        <div className={`card-body ${selected.includes(course) ? 'selected' : ''} ${disabled.includes(course) ? 'disabled' : ''}`}>
         <h5 className="card-title">{course.term} CS {course.number}</h5>
         <p className="card-text">{course.title}</p>
         <hr />
--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -1,10 +1,9 @@
 import Course from './Course';
 
-const CourseList = ({courses, selected, toggleSelected}) => {
-  return(
+const CourseList = ({courses, selected, toggleSelected, disabled}) => (
   <div className="course-list">
-    { courses.map(([id, course]) => <Course key={id} id={id} course={course} selected={selected} toggleSelected={toggleSelected}/>) }
+    { courses.map(([id, course]) => <Course key={id} id={id} course={course} selected={selected} toggleSelected={toggleSelected} disabled={disabled}/>) }
   </div>
-)};
+);
 
 export default CourseList;
