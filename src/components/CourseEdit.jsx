b56422231f97235dc138ce39481f26febdacc85b
Cynthia <Liangshao2023@u.northwestern.edu>
2022-10-07 21:01:54 -0500

    add form validation

--- src/components/CourseEdit.jsx
+++ src/components/CourseEdit.jsx
@@ -1,9 +1,25 @@
+import { useFormData } from '../utilities/useFormData';
+
 const InputField = ({name, text, state, change}) => (
   <div className="mb-3">
     <label htmlFor={name} className="form-label">{text}</label>
-    <input className="form-control" id={name} name={name} />
+    <input className="form-control" id={name} name={name} 
+      defaultValue={state.values?.[name]} onChange={change} />
+    <div className="invalid-feedback">{state.errors?.[name]}</div>
   </div>
 );
+
+const validateCourseData = (key, val) => {
+    switch (key) {
+        case 'title':
+            return /(^\w\w)/.test(val) ? '' : 'must be least two characters';
+
+        case 'meets':
+            return /([0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]-([0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]/ .test(val) ? '' : 'must contain days and start-end, e.g., MWF 12:00-13:20';
+            
+        default: return '';
+    }
+};
 
 const ButtonBar = ({message, disabled}) => {
     return (
@@ -15,17 +31,19 @@ const ButtonBar = ({message, disabled}) => {
 };
 
 const CourseEdit = ({data}) => {
-  const submit = (evt) => {
-    evt.preventDefault();
-  };
+    const [state, change] = useFormData(validateCourseData, data);
 
-  return (
-    <form onSubmit={submit} noValidate>
-      <InputField name="title" text="Title"/>
-      <InputField name="meetingTime" text="Meeting Time"/>
-      <ButtonBar />
-    </form>
-  )
+    const submit = (evt) => {
+        evt.preventDefault();
+    };
+
+    return (
+        <form onSubmit={submit} noValidate className={state.errors ? 'was-validated' : null}>
+        <InputField name="title" text="Title" state={state} change={change}/>
+        <InputField name="meets" text="Meeting Time" state={state} change={change}/>
+        <ButtonBar />
+        </form>
+    )
 };
 
 export default CourseEdit;
